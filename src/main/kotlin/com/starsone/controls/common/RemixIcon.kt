12b3f3f4127ef578c3ca9a52ecc23f3276f930c4
starone <starsone@Foxmail.com>
2022-10-15 14:42:24 +0800

    :sparkles: 新增RemixIcon字体图标组件

---
+++ src/main/kotlin/com/starsone/controls/common/RemixIcon.kt
@@ -0,0 +1,67 @@
+package com.starsone.controls.common
+
+import javafx.event.EventTarget
+import javafx.scene.control.Label
+import javafx.scene.paint.Paint
+import javafx.scene.text.Text
+import org.apache.commons.text.StringEscapeUtils
+import tornadofx.*
+
+object RemixIconData {
+    val iconIndexMap = hashMapOf<String, String>()
+
+    val jsonPath = "/ttf/remixicon.json"
+    val fontPath = "/ttf/remixicon.ttf"
+
+    fun init(resources: ResourceLookup) {
+        //加载图标字体文件
+        val jsonArray = loadJsonArray(resources.text(jsonPath))
+
+        val iterator = jsonArray.iterator()
+        while (iterator.hasNext()) {
+            val jsonObject = iterator.next().asJsonObject()
+            val key = jsonObject.getString("name")
+            val value = jsonObject.getString("unicode")
+            iconIndexMap[key] = StringEscapeUtils.unescapeJava(value)
+        }
+    }
+
+}
+
+/**
+ * Remix字体图标Text控件,使用时候需要在Application中进行初始化数据操作
+ * @param name    图标参考地址 https://remixicon.com,目前使用的remix icon 2.5.0版本
+ * @param fontColor 字体颜色,默认是黑色
+ * @param fontSize 字体大小,默认为25px
+ * @param op
+ * @return
+ */
+fun EventTarget.remixIconText(name: String, fontColor: Paint = c("black"), fontSize: Int = 25, op: (Text.() -> Unit) = {}): Text {
+    val text = text {
+        font = loadFont(RemixIconData.fontPath, fontSize)!!
+        text = RemixIconData.iconIndexMap[name]
+        style {
+            fill = fontColor
+        }
+    }
+    return opcr(this, text, op)
+}
+
+/**
+ * Remix字体图标Label控件,使用时候需要在Application中进行初始化数据操作
+ * @param name    图标参考地址 https://remixicon.com,目前使用的remix icon 2.5.0版本
+ * @param fontColor 字体颜色,默认是黑色
+ * @param fontSize 字体大小,默认为25px
+ * @param op
+ * @return
+ */
+fun EventTarget.remixIconLabel(name: String, fontColor: Paint = c("black"), fontSize: Int = 25, op: (Label.() -> Unit) = {}): Label {
+    val label = label {
+        font = loadFont(RemixIconData.fontPath, fontSize)!!
+        text = RemixIconData.iconIndexMap[name]
+        style {
+            textFill = fontColor
+        }
+    }
+    return opcr(this, label, op)
+}
