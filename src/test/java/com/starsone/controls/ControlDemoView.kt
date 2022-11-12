3eaf9d6c7e29d782b8b5cf51ea5c95b31ff5edd3
starone <starsone@Foxmail.com>
2022-11-12 20:23:17 +0800

    :white_check_mark: 选择文件夹的组件效果测试

---
+++ src/test/java/com/starsone/controls/ControlDemoView.kt
@@ -0,0 +1,34 @@
+package com.starsone.controls
+
+import com.starsone.controls.common.RemixIconData
+import com.starsone.controls.common.remixIconText
+import com.starsone.controls.common.xChooseFileDirectory
+import javafx.beans.property.SimpleStringProperty
+import kfoenix.jfxbutton
+import tornadofx.*
+
+/**
+ * @author StarsOne
+ * @url <a href="http://stars-one.site">http://stars-one.site</a>
+ * @date create in  2020/11/19 10:21
+ */
+class ControlDemoView : View("My View") {
+
+    val dirPath = SimpleStringProperty("")
+    override val root = vbox {
+        setPrefSize(600.0, 400.0)
+        style {
+            backgroundColor += c("white")
+        }
+        RemixIconData.init(resources)
+        xChooseFileDirectory("下载目录", dirPath)
+
+        xChooseFileDirectory("下载目录", dirPath, jfxbutton {
+            graphic = remixIconText("folder-2-fill")
+            text = "选择文件夹"
+        })
+
+
+
+    }
+}
