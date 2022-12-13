91e4a0813b8349201eea887a7ed0658bc34a33ed
starone <starsone@Foxmail.com>
2022-12-14 00:50:01 +0800

    :sparkles: 新增弹窗可以绑定任意Pane

--- src/main/kotlin/com/starsone/controls/view/XMessage.kt
+++ src/main/kotlin/com/starsone/controls/view/XMessage.kt
@@ -8,19 +8,18 @@ import javafx.event.ActionEvent
 import javafx.event.EventHandler
 import javafx.geometry.Pos
 import javafx.scene.Node
-import javafx.scene.layout.StackPane
-import javafx.scene.layout.VBox
+import javafx.scene.layout.*
 import javafx.util.Duration
+import tornadofx.*
 import java.util.concurrent.TimeUnit
 
 /**
- * @author ChenFei
- * @date 2022年7月22日
- *
- *
- * CFMessage 消息提示
+ * XMessage 消息提示
+ * 参考 [chenfei-javafx-css项目的CFMessage](https://gitee.com/lichenfei_fei/chenfei-javafx-css/blob/control/src/main/java/cn/chenfei/jfx/css/control/CFMessage.java),增加了调整顺序和绑定任意pane的功能
+ * @author stars-one
+ * @date 2022-12-14 00:43:07
  */
-class CFMessage private constructor(stackPane: StackPane) {
+class XMessage private constructor(stackPane: StackPane) {
 
     private val messageBox = VBox()
 
@@ -114,9 +113,48 @@ class CFMessage private constructor(stackPane: StackPane) {
          * @param isTop 通知的出现顺序 true:从上往下 false:从下往上
          * @return
          */
-        fun bindingContainer(stackPane: StackPane, isTop: Boolean = true): CFMessage {
+        private fun bindingContainer(stackPane: StackPane, isTop: Boolean = true): XMessage {
             this.isTop = isTop
-            return CFMessage(stackPane)
+            return XMessage(stackPane)
+        }
+
+        /**
+         * 绑定容器，一般可以定义一个顶级的容器来存放消息提示
+         *
+         * @param pane 任意一个pane即可
+         * @param isTop 通知的出现顺序 true:从上往下 false:从下往上
+         * @return
+         */
+        fun bindingContainer(pane: Pane, isTop: Boolean = true): XMessage {
+            this.isTop = isTop
+            //将vbox里面的节点弄出来,存放在tempVbox
+
+            if (pane is StackPane) {
+                return bindingContainer(pane, isTop)
+            } else {
+                //如果绑定的不是stackpane
+                //    1.则判断原来的pane是何种类型,将其所以节点移到同类型的一个tempPane
+                //    2.创建stackpane,将tempPane节点放入其中
+                //    2.清空原pane的所有节点,将上面创建的stackpane节点添加进去
+                val tempPane = when (pane) {
+                    is VBox -> VBox()
+                    is HBox -> HBox()
+                    is StackPane -> StackPane()
+                    is AnchorPane -> AnchorPane()
+                    else -> StackPane()
+                }
+                val nodeList = pane.children
+
+                tempPane.children.addAll(nodeList)
+                //清除掉所有节点
+                pane.clear()
+                //将tempVbox使用stackPane包裹起来
+                val stackPane = StackPane()
+                stackPane.add(tempPane)
+
+                pane.add(stackPane)
+                return XMessage(stackPane)
+            }
         }
     }
 
