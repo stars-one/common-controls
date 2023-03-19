2c57c21eecd91fa48a9c4aaddf9f3031d1becb21
starone <starsone@Foxmail.com>
2023-03-19 20:13:54 +0800

    :sparkles: remix的相关组件方法可传SimpleStringProperty对象,方便有时需要更改字体图标

--- src/main/kotlin/com/starsone/controls/common/RemixIcon.kt
+++ src/main/kotlin/com/starsone/controls/common/RemixIcon.kt
@@ -1,6 +1,7 @@
 package com.starsone.controls.common
 
 import com.jfoenix.controls.JFXButton
+import javafx.beans.value.ObservableValue
 import javafx.event.EventTarget
 import javafx.scene.control.Label
 import javafx.scene.paint.Paint
@@ -76,6 +77,50 @@ fun EventTarget.remixIconText(name: String, fontColor: Paint = c("black"), fontS
 }
 
 /**
+ * Remix字体图标Text控件,使用时候需要在Application中进行初始化数据操作
+ * @param observable    可变的图标名,参考地址 https://remixicon.com,目前使用的remix icon 2.5.0版本
+ * @param fontColor 字体颜色,默认是黑色
+ * @param fontSize 字体大小,默认为25px
+ * @param op
+ * @return
+ */
+fun EventTarget.remixIconText(observable: ObservableValue<String>, fontColor: Paint = c("black"), fontSize: Int = 25, op: (Text.() -> Unit) = {}): Text {
+    val text = text {
+        font = loadFont(RemixIconData.fontPath, fontSize)!!
+        observable.addListener { observable, oldValue, newValue ->
+            text = RemixIconData.iconIndexMap[newValue]
+        }
+        text = RemixIconData.iconIndexMap[observable.value]
+        style {
+            fill = fontColor
+        }
+    }
+    return opcr(this, text, op)
+}
+
+/**
+ * Remix字体图标Label控件,使用时候需要在Application中进行初始化数据操作
+ * @param observable    可变的图标名,参考地址 https://remixicon.com,目前使用的remix icon 2.5.0版本
+ * @param fontColor 字体颜色,默认是黑色
+ * @param fontSize 字体大小,默认为25px
+ * @param op
+ * @return
+ */
+fun EventTarget.remixIconLabel(observable: ObservableValue<String>, fontColor: Paint = c("black"), fontSize: Int = 25, op: (Label.() -> Unit) = {}): Label {
+    val label = label {
+        font = loadFont(RemixIconData.fontPath, fontSize)!!
+        observable.addListener { _, _, newValue ->
+            text = RemixIconData.iconIndexMap[newValue]
+        }
+        text = RemixIconData.iconIndexMap[observable.value]
+        style {
+            textFill = fontColor
+        }
+    }
+    return opcr(this, label, op)
+}
+
+/**
  * Remix字体图标Label控件,使用时候需要在Application中进行初始化数据操作
  * @param name    图标参考地址 https://remixicon.com,目前使用的remix icon 2.5.0版本
  * @param fontColor 字体颜色,默认是黑色
@@ -112,6 +157,24 @@ fun EventTarget.remixIconButtonWithBorder(name: String, fontColor: Paint = c("bl
 }
 
 /**
+ * Remix圆形图标按钮(设置边框与文本颜色一致)
+ *
+ * @param observable 可变的图标名称
+ * @param fontColor 文本颜色
+ * @param fontSize 文本字体大小
+ * @param bgColor 按钮背景色
+ * @param buttonType 按钮类型
+ * @param op
+ * @receiver
+ * @return
+ */
+fun EventTarget.remixIconButtonWithBorder(observable: ObservableValue<String>, fontColor: Paint = c("black"), fontSize: Int = 25, bgColor: Paint = c("white"), buttonType: JFXButton.ButtonType = JFXButton.ButtonType.RAISED, op: (JFXButton.() -> Unit) = {}): JFXButton {
+    val btn = xIconButton(remixIconLabel(observable, fontColor, fontSize), bgColor, fontColor, buttonType, fontColor)
+    return opcr(this, btn, op)
+}
+
+
+/**
  * Remix圆形图标按钮
  *
  * @param name 图标名称
@@ -128,3 +191,19 @@ fun EventTarget.remixIconButton(name: String, fontColor: Paint = c("black"), fon
     return opcr(this, btn, op)
 }
 
+/**
+ * Remix圆形图标按钮
+ *
+ * @param observable 可变的图标名称
+ * @param fontColor 文本颜色
+ * @param fontSize 文本字体大小
+ * @param bgColor 按钮背景色
+ * @param buttonType 按钮类型
+ * @param op
+ * @receiver
+ * @return
+ */
+fun EventTarget.remixIconButton(observable: ObservableValue<String>, fontColor: Paint = c("black"), fontSize: Int = 25, bgColor: Paint = c("white"), buttonType: JFXButton.ButtonType = JFXButton.ButtonType.RAISED, op: (JFXButton.() -> Unit) = {}): JFXButton {
+    val btn = xIconButton(remixIconLabel(observable, fontColor, fontSize), bgColor, fontColor, buttonType, null)
+    return opcr(this, btn, op)
+}
