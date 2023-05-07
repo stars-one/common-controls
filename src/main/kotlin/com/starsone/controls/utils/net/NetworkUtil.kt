2d1a987e98ddc2e6f60a08c66bd5fcda0bc2cb27
starone <starsone@Foxmail.com>
2023-05-07 16:02:21 +0800

    :sparkles: 网络请求工具类增加同步的api方法

--- src/main/kotlin/com/starsone/controls/utils/net/NetworkUtil.kt
+++ src/main/kotlin/com/starsone/controls/utils/net/NetworkUtil.kt
@@ -46,7 +46,7 @@ object NetworkUtil {
     }
 
     /**
-     * 发起post请求(传统的表单)
+     * 发起post请求(传统的表单),异步
      *
      * @param url 接口地址
      * @param data 数据HashMap
@@ -75,7 +75,35 @@ object NetworkUtil {
     }
 
     /**
-     * Post请求(传json数据)
+     * 发起post请求(传统的表单)
+     *
+     * @param url 接口地址
+     * @param data 数据HashMap
+     * @param headers 请求头 HashMap
+     */
+    fun post(url: String, data: HashMap<String, out Any>, headers: HashMap<String, String>): Response {
+        //1.构造传参参数
+        val formBody = FormBody.Builder()
+        data.forEach { (t, v) ->
+            formBody.add(t, v.toString())
+        }
+        val formBodyData = formBody.build()
+        //2.构造request
+        val requestBuilder = Request.Builder()
+                .url(url)
+                .post(formBodyData)
+
+        //构造头部参数
+        headers.forEach { (t, u) ->
+            requestBuilder.addHeader(t, u)
+        }
+        val request = requestBuilder.build()
+        //3.发送请求
+        return sendRequest(request)
+    }
+
+    /**
+     * Post请求(传json数据) 异步
      *
      * @param T
      * @param url
@@ -102,7 +130,33 @@ object NetworkUtil {
     }
 
     /**
-     * Get 请求
+     * Post请求(传json数据)
+     *
+     * @param T
+     * @param url
+     * @param data 实体类数据(会自动转为json字符串)
+     * @param headers 请求头
+     */
+    fun postJson(url: String, data: Any, headers: HashMap<String, String>): Response {
+        //1.构造传参参数
+        val requestBody = gson.toJson(data).toRequestBody(JSON)
+
+        //2.构造request
+        val requestBuilder = Request.Builder()
+                .url(url)
+                .post(requestBody)
+
+        //构造头部参数
+        headers.forEach { (t, u) ->
+            requestBuilder.addHeader(t, u)
+        }
+        val request = requestBuilder.build()
+        //3.发送请求
+        return sendRequest(request)
+    }
+
+    /**
+     * Get 请求 异步
      *
      * @param url
      * @param data 数据map,会自动拼接
@@ -130,6 +184,33 @@ object NetworkUtil {
         sendRequest(request, callBack)
     }
 
+    /**
+     * Get 请求
+     *
+     * @param url
+     * @param data 数据map,会自动拼接
+     * @param headers
+     */
+    fun get(url: String, data: HashMap<String, out Any>, headers: HashMap<String, String>): Response {
+        //1.构造传参参数
+        val param = data.map { (t, u) ->
+            """$t=$u"""
+        }.joinToString("&")
+
+        var finalUrl = url + param
+        if (!url.endsWith("?")) {
+            finalUrl = url + "?" + param
+        }
+        val builder = Request.Builder()
+                .url(finalUrl)
+        headers.forEach { (t, u) ->
+            builder.addHeader(t, u)
+        }
+        //2.构造request
+        val request = builder.get().build()
+        //3.发出请求
+        return sendRequest(request)
+    }
 
     /**
      * 发起网络请求
@@ -161,6 +242,12 @@ object NetworkUtil {
             }
         })
     }
+
+
+    fun sendRequest(request: Request): Response {
+        return okHttpClient.newCall(request).execute()
+    }
+
 
 
     /**
