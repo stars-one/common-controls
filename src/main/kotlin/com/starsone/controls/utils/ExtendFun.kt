80d58154a3baa56916ff04170cf7952c17b4d7e2
starone <starsone@Foxmail.com>
2023-11-05 12:48:57 +0800

    :sparkles: 新增toUnitStringNew和toUnitStringNewWithUnit方法

--- src/main/kotlin/com/starsone/controls/utils/ExtendFun.kt
+++ src/main/kotlin/com/starsone/controls/utils/ExtendFun.kt
@@ -6,6 +6,8 @@ import java.math.BigDecimal
 import java.text.DecimalFormat
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.math.log10
+import kotlin.math.pow
 
 
 fun Long.toDateString(format: String = "yyyy-MM-dd HH:mm:ss"): String {
@@ -17,7 +19,7 @@ fun Date.toDateString(format: String = "yyyy-MM-dd HH:mm:ss"): String {
 }
 
 /**
- * 将字节(B)转为对应的单位
+ * 将字节(B)转为对应的单位(保留2位小数)
  */
 fun Long.toUnitString(): String {
     val df = DecimalFormat("#.00")
@@ -27,6 +29,75 @@ fun Long.toUnitString(): String {
         bytes < 1048576 -> df.format(bytes.toDouble() / 1024) + "K"
         bytes < 1073741824 -> df.format(bytes.toDouble() / 1048576) + "Mb"
         else -> df.format(bytes.toDouble() / 1073741824) + "Gb"
+    }
+}
+
+/**
+ * 将字节(B)转为对应的单位(保留2位小数),最大单位为GB
+ * @param numUnit 最后输出保留[numUnit]位小数
+ */
+fun Long.toUnitStringNew(numUnit: Int = 1): String {
+    val size = this
+
+    if (size <= 0) {
+        return "0 B"
+    }
+
+    val units = arrayOf("B", "KB", "MB", "GB")
+    val digitGroups = (log10(size.toDouble()) / log10(1024.0)).toInt()
+
+    //处理逻辑,如果大于100KB,则转为0.1MB,大于100MB,转为0.1GB
+    for (i in 2 downTo 0) {
+        if (size > 100 * (1024.0.pow(i))) {
+            if (digitGroups >= i) {
+                val formattedSize = String.format("%.${numUnit}f", size / 1024.0.pow((i + 1) * 1.0))
+                return "$formattedSize ${units[i + 1]}"
+            }
+        }
+    }
+
+    return if (digitGroups == 0) {
+        //单位为B,不保留小数点
+        String.format("%.0f %s", size.toFloat(), units[digitGroups])
+    } else {
+        val formattedSize =
+                String.format("%.${numUnit}f", size / 1024.0.pow(digitGroups.toDouble()))
+        "$formattedSize ${units[digitGroups]}"
+    }
+}
+
+/**
+ * 将字节(B)转为对应的单位Pair(数值和单位分开),最大单位为GB
+ * @param numUnit 最后输出保留[numUnit]位小数
+ * @return Pair<数值,单位>
+ */
+fun Long.toUnitStringNewWithUnit(numUnit: Int = 1): Pair<String, String> {
+    val size = this
+
+    if (size <= 0) {
+        return Pair("0", "B")
+    }
+
+    val units = arrayOf("B", "KB", "MB", "GB")
+    val digitGroups = (log10(size.toDouble()) / log10(1024.0)).toInt()
+
+    //处理逻辑,如果大于100KB,则转为0.1MB,大于100MB,转为0.1GB
+    for (i in 2 downTo 0) {
+        if (size > 100 * (1024.0.pow(i))) {
+            if (digitGroups >= i) {
+                val formattedSize = String.format("%.${numUnit}f", size / 1024.0.pow((i + 1) * 1.0))
+                return Pair(formattedSize, units[i + 1])
+            }
+        }
+    }
+
+    return if (digitGroups == 0) {
+        //单位为B,不保留小数点
+        Pair(String.format("%.0f %s", size.toFloat(), units[digitGroups]), units[digitGroups])
+    } else {
+        val formattedSize =
+                String.format("%.${numUnit}f", size / 1024.0.pow(digitGroups.toDouble()))
+        Pair(formattedSize, units[digitGroups])
     }
 }
 
